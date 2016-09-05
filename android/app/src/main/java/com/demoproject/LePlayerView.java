34c70f9c766b135d6e32f6b68e342932b6561c3e
littlethree <jiarao@gmail.com>
2016-09-05 19:56:45 +0800

    导入LeSDK

---
+++ android/app/src/main/java/com/demoproject/LePlayerView.java
@@ -0,0 +1,27 @@
+package com.demoproject;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.RelativeLayout;
+
+/**
+ * Created by raojia on 16/9/5.
+ */
+public class LePlayerView extends RelativeLayout {
+
+    public LePlayerView(Context context) {
+        super(context);
+    }
+
+    public LePlayerView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public LePlayerView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public LePlayerView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+}
