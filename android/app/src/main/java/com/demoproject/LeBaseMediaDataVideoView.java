621a3c6d5be7a2859bdafaf39ceca134f7bd44f9
littlethree <jiarao@gmail.com>
2016-11-02 01:06:27 +0800

    添加码率切换

---
+++ android/app/src/main/java/com/demoproject/LeBaseMediaDataVideoView.java
@@ -0,0 +1,10 @@
+package com.demoproject;
+
+import com.letv.android.client.sdk.videoview.base.BaseMediaDataVideoView;
+
+/**
+ * Created by raojia on 2016/11/1.
+ */
+
+public class LeBaseMediaDataVideoView extends BaseMediaDataVideoView {
+}
