d70f32469b18e615568b7acf8eb8d567e9f1862c
littlethree <jiarao@gmail.com>
2016-12-09 03:47:22 +0800

    添加水印成功！

---
+++ android/app/src/com/lecloud/DemoProject/leecoSdk/DrawingThread.java
@@ -0,0 +1,8 @@
+package com.lecloud.DemoProject.leecoSdk;
+
+/**
+ * Created by LizaRao on 2016/12/9.
+ */
+
+public class DrawingThread {
+}
---
+++ android/app/src/com/lecloud/DemoProject/leecoSdk/watermark/WaterMarkSurfaceView.java
@@ -0,0 +1,10 @@
+package com.lecloud.DemoProject.leecoSdk.watermark;
+
+import android.view.SurfaceView;
+
+/**
+ * Created by LizaRao on 2016/12/9.
+ */
+
+public class WaterMarkSurfaceView extends SurfaceView {
+}
