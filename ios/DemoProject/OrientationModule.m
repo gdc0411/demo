48c1cd365c49277bc14de34eb5b8fa1afa33fb98
littlethree <raojia@LizaRaodeMac.local>
2016-12-11 02:37:31 +0800

    添加全局转屏支持

---
+++ ios/DemoProject/OrientationModule.m
@@ -0,0 +1,13 @@
+//
+//  OrientationModule.m
+//  DemoProject
+//
+//  Created by LizaRao on 2016/12/11.
+//  Copyright © 2016年 Facebook. All rights reserved.
+//
+
+#import "OrientationModule.h"
+
+@implementation OrientationModule
+
+@end
