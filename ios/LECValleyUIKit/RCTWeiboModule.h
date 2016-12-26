f4ca38667b129560f3f2d106e07462d6c2ff96aa
littlethree <raojia@LizaRaodeMac.local>
2016-12-27 03:59:19 +0800

    添加iOS版微博支持

---
+++ ios/LECValleyUIKit/RCTWeiboModule.h
@@ -0,0 +1,13 @@
+//
+//  RCTWeiboModule.h
+//  LECValleyUIKit
+//
+//  Created by LizaRao on 2016/12/27.
+//  Copyright © 2016年 Facebook. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RCTWeiboModule : NSObject
+
+@end
---
+++ ios/LECValleyUIKit/RCTWeiboModule.m
@@ -0,0 +1,13 @@
+//
+//  RCTWeiboModule.m
+//  LECValleyUIKit
+//
+//  Created by LizaRao on 2016/12/27.
+//  Copyright © 2016年 Facebook. All rights reserved.
+//
+
+#import "RCTWeiboModule.h"
+
+@implementation RCTWeiboModule
+
+@end
