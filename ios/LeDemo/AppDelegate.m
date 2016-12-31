b525af3cec9f40a79b93bcc23adbfc5bfa8b207b
littlethree <jiarao@gmail.com>
2016-12-31 18:33:42 +0800

    集成友盟推送

--- ios/LeDemo/AppDelegate.m
+++ ios/LeDemo/AppDelegate.m
@@ -10,6 +10,8 @@
 #import "AppDelegate.h"
 #import "CodePush.h"
 
+#import "UMessage.h"
+
 #import "RCTBundleURLProvider.h"
 #import "RCTRootView.h"
 
@@ -44,11 +46,39 @@
   jsCodeLocation = [CodePush bundleURL];
 #endif
   
-  
   /*
    确保App启动的屏幕方向
    */
   [[UIApplication sharedApplication] setStatusBarOrientation:(UIInterfaceOrientationPortrait) animated:NO];
+  
+  /*
+   *******************************************************************
+   *******************************************************************
+   友盟功能导入:
+   */
+  //初始化方法,也可以使用(void)startWithAppkey:(NSString *)appKey launchOptions:(NSDictionary * )launchOptions httpsenable:(BOOL)value;这个方法，方便设置https请求。
+  [UMessage startWithAppkey:@"586781631c5dd0359f001286" launchOptions:launchOptions];
+  
+  //注册通知，如果要使用category的自定义策略，可以参考demo中的代码。
+  [UMessage registerForRemoteNotifications];
+  
+  //iOS10必须加下面这段代码。
+  UNUserNotificationCenter *center = [UNUserNotificationCenter currentNotificationCenter];
+  center.delegate = self;
+  UNAuthorizationOptions types10 = UNAuthorizationOptionBadge|UNAuthorizationOptionAlert|UNAuthorizationOptionSound;
+  [center requestAuthorizationWithOptions:types10
+                        completionHandler:^(BOOL granted, NSError * _Nullable error) {
+                          if (granted) {
+                            //点击允许
+                            //这里可以添加一些自己的逻辑
+                          } else {
+                            //点击不允许
+                            //这里可以添加一些自己的逻辑
+                          }
+                        }];
+  //打开日志，方便调试
+  [UMessage setLogEnabled:YES];
+  
   
   /*
    *******************************************************************
@@ -80,13 +110,13 @@
   [[LECPlayerFoundation sharedFoundation]fileLogEnable:NO];
   [[LECPlayerFoundation sharedFoundation] consoleLogEnable:YES];
   
-
+  
   RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
                                                       moduleName:@"LeDemo"
                                                initialProperties:nil
                                                    launchOptions:launchOptions];
   rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
-
+  
   self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
   UIViewController *rootViewController = [UIViewController new];
   rootViewController.view = rootView;
@@ -95,6 +125,66 @@
   [self.window makeKeyAndVisible];
   
   return YES;
+}
+
+
+//iOS10以下使用这个方法接收通知
+- (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo
+{
+  //关闭友盟自带的弹出框
+  [UMessage setAutoAlert:NO];
+  [UMessage didReceiveRemoteNotification:userInfo];
+  
+  //    self.userInfo = userInfo;
+  //    //定制自定的的弹出框
+  //    if([UIApplication sharedApplication].applicationState == UIApplicationStateActive)
+  //    {
+  //        UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"标题"
+  //                                                            message:@"Test On ApplicationStateActive"
+  //                                                           delegate:self
+  //                                                  cancelButtonTitle:@"确定"
+  //                                                  otherButtonTitles:nil];
+  //
+  //        [alertView show];
+  //
+  //    }
+}
+
+
+//iOS10新增：处理前台收到通知的代理方法
+-(void)userNotificationCenter:(UNUserNotificationCenter *)center
+      willPresentNotification:(UNNotification *)notification
+        withCompletionHandler:(void (^)(UNNotificationPresentationOptions))completionHandler{
+  
+  NSDictionary * userInfo = notification.request.content.userInfo;
+  if([notification.request.trigger isKindOfClass:[UNPushNotificationTrigger class]]) {
+    //应用处于前台时的远程推送接受
+    //关闭友盟自带的弹出框
+    [UMessage setAutoAlert:NO];
+    //必须加这句代码
+    [UMessage didReceiveRemoteNotification:userInfo];
+    
+  }else{
+    //应用处于前台时的本地推送接受
+  }
+  //当应用处于前台时提示设置，需要哪个可以设置哪一个
+  completionHandler(UNNotificationPresentationOptionSound|UNNotificationPresentationOptionBadge|UNNotificationPresentationOptionAlert);
+}
+
+//iOS10新增：处理后台点击通知的代理方法
+-(void)userNotificationCenter:(UNUserNotificationCenter *)center
+didReceiveNotificationResponse:(UNNotificationResponse *)response
+        withCompletionHandler:(void (^)())completionHandler{
+  
+  NSDictionary * userInfo = response.notification.request.content.userInfo;
+  if([response.notification.request.trigger isKindOfClass:[UNPushNotificationTrigger class]]) {
+    //应用处于后台时的远程推送接受
+    //必须加这句代码
+    [UMessage didReceiveRemoteNotification:userInfo];
+    
+  }else{
+    //应用处于后台时的本地推送接受
+  }
 }
 
 
