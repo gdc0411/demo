77bb37047e3ad74b837ea87aca2e730d280d176e
littlethree <jiarao@gmail.com>
2016-12-31 18:56:01 +0800

    增加离线包

--- ios/LeDemo/AppDelegate.m
+++ ios/LeDemo/AppDelegate.m
@@ -41,6 +41,7 @@
   NSURL *jsCodeLocation;
   
 #ifdef DEBUG
+//    jsCodeLocation = [[NSBundle mainBundle] URLForResource:@"index.ios" withExtension:@"jsbundle"];
   jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index.ios" fallbackResource:nil];
 #else
   jsCodeLocation = [CodePush bundleURL];
