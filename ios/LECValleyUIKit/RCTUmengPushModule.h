07dd19e52afc0d1ec99f935fd011d13672e25fe0
littlethree <jiarao@gmail.com>
2017-01-11 20:35:28 +0800

    iOS推送

--- ios/LECValleyUIKit/RCTUmengPushModule.h
+++ ios/LECValleyUIKit/RCTUmengPushModule.h
@@ -3,11 +3,15 @@
 //  LECValleyUIKit
 //
 //  Created by RaoJia on 2017/1/11.
-//  Copyright © 2017年 Facebook. All rights reserved.
+//  Copyright © 2017年 leCloud. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
+#import <React/RCTBridgeModule.h>
 
-@interface RCTUmengPushModule : NSObject
+@interface RCTUmengPushModule : NSObject<RCTBridgeModule>
+
++ (void)registerWithAppkey:(NSString *)appkey launchOptions:(NSDictionary *)launchOptions;
++ (void)application:(UIApplication *)application didRegisterDeviceToken:(NSData *)deviceToken;
++ (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo;
 
 @end
