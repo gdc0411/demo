e0035f48f2dea300be5031388bede18ca999668b
littlethree <raojia@LizaRaodeMac.local>
2017-01-26 05:12:43 +0800

    添加iOS下载接口

---
+++ ios/LECValleyUIKit/RCTDownloadModule.h
@@ -0,0 +1,13 @@
+//
+//  RCTDownloadModule.h
+//  LECValleyUIKit
+//
+//  Created by LizaRao on 2017/1/26.
+//  Copyright © 2017年 Facebook. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RCTDownloadModule : NSObject
+
+@end
---
+++ ios/LECValleyUIKit/RCTDownloadModule.m
@@ -0,0 +1,13 @@
+//
+//  RCTDownloadModule.m
+//  LECValleyUIKit
+//
+//  Created by LizaRao on 2017/1/26.
+//  Copyright © 2017年 Facebook. All rights reserved.
+//
+
+#import "RCTDownloadModule.h"
+
+@implementation RCTDownloadModule
+
+@end
