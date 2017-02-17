3105e7fdedac928fc4abf4f7c50a654486bdbb5f
littlethree <jiarao@gmail.com>
2017-02-18 05:28:24 +0800

    完成ImagePicker

---
+++ ios/LECValleyUIKit/RCTImagePickerModule.h
@@ -0,0 +1,13 @@
+//
+//  RCTImagePickerModule.h
+//  LECValleyUIKit
+//
+//  Created by LizaRao on 2017/2/18.
+//  Copyright © 2017年 Facebook. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RCTImagePickerModule : NSObject
+
+@end
---
+++ ios/LECValleyUIKit/RCTImagePickerModule.m
@@ -0,0 +1,13 @@
+//
+//  RCTImagePickerModule.m
+//  LECValleyUIKit
+//
+//  Created by LizaRao on 2017/2/18.
+//  Copyright © 2017年 Facebook. All rights reserved.
+//
+
+#import "RCTImagePickerModule.h"
+
+@implementation RCTImagePickerModule
+
+@end
