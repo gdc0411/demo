df53cec4305c7c29b7864c035c3842f070402c8b
littlethree <jiarao@gmail.com>
2017-02-27 02:24:50 +0800

    带剪裁的图片抓取

---
+++ android/LeValleyLibrary/src/main/java/com/lecloud/valley/modules/CropPickerFunc.java
@@ -0,0 +1,8 @@
+package com.lecloud.valley.modules;
+
+/**
+ * Created by LizaRao on 2017/2/27.
+ */
+
+public class CropPickerFunc {
+}
---
+++ android/LeValleyLibrary/src/main/java/com/lecloud/valley/utils/ResultCollector.java
@@ -0,0 +1,8 @@
+package com.lecloud.valley.utils;
+
+/**
+ * Created by LizaRao on 2017/2/27.
+ */
+
+public class ResultCollector {
+}
