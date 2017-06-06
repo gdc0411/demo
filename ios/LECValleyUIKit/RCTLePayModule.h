50628632f51b9362a9c75f014d918cb75649555f
littlethree <jiarao@gmail.com>
2017-06-06 21:11:03 +0800

    支持boss

---
+++ ios/LECValleyUIKit/RCTLePayModule.h
@@ -0,0 +1,13 @@
+//
+//  RCTLePayModule.h
+//  LECValleyUIKit
+//
+//  Created by RaoJia on 2017/6/6.
+//  Copyright © 2017年 LeCloud. All rights reserved.
+//
+
+#import <React/RCTBridgeModule.h>
+
+@interface RCTLePayModule : NSObject<RCTBridgeModule>
+
+@end
