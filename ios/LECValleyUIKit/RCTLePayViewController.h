e1f1d3e476c91c544dcdaee4030a752efc43a41a
littlethree <jiarao@gmail.com>
2017-06-21 16:43:33 +0800

    update

---
+++ ios/LECValleyUIKit/RCTLePayViewController.h
@@ -0,0 +1,13 @@
+//
+//  RCTLePayViewController.h
+//  LECValleyUIKit
+//
+//  Created by RaoJia on 2017/6/19.
+//  Copyright © 2017年 Facebook. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RCTLePayViewController : UIViewController
+
+@end
---
+++ ios/LECValleyUIKit/RCTLePayViewControllerDelegate.h
@@ -0,0 +1,13 @@
+//
+//  RCTLePayViewControllerDelegate.h
+//  LECValleyUIKit
+//
+//  Created by RaoJia on 2017/6/19.
+//  Copyright © 2017年 Facebook. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol RCTLePayViewControllerDelegate <NSObject>
+
+@end
